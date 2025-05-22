b698cb9583d66bee33e52cefba8bd31c80ab0528
Taha Ahmad <craftingcaptain456@gmail.com>
2025-05-22 21:30:33 +0500

    ofc hash changed in the second I updated it, will make this an input

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -6,7 +6,7 @@
     (import (
       builtins.fetchTarball {
         url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-        sha256 = "1bq7phb5hqczbpf5pgi7gm0f9qb0hhzgm7rwhyfb752dgndvackn";
+        sha256 = "18vy5q57hvh74mzpkfp8q6xl36139wfp6hcr2m51fcfx6qslf9lj";
       }
     ))
   ];
